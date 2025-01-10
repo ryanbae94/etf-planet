130083f7c6c0fa7cb28c54d8b0648b3622a6d87b
ryanbae94 <ryanbae94@gmail.com>
2025-01-10 18:01:06 +0900

    feat: 우주 배경을 레이아웃으로 변경

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,9 @@
 import Search from './components/Search';
-import StarLayer from './components/StarLayer';
 import * as styles from './style/Home/page.css';
 
 export default function Home() {
 	return (
 		<>
-			<StarLayer />
 			<div className={styles.container}>
 				<div className={styles.centerBox}>
 					<h1>ETF Planet</h1>
